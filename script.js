a0e9dc728c38bd048ff750595b13fd1310999c76
Rachasak Ragkamnerd <id513128@gmail.com>
2017-10-01 23:20:24 +0700

    Add saturate increasing style for PPTV video player.

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ javascript: (function () {
   for( index=0; index < elem.length; index++ ) {
        removeGrayscale(elem[index]);
   }
+  if(document.location.origin == "https://www.pptvhd36.com"){
+  	var elem = document.querySelectorAll([".js-player",".jw-media"]); 
+	for( index=0; index < elem.length; index++ ) {
+	    elem[index].style.setProperty("filter", "saturate(4)", "important");
+	}
+  }
+
   function removeGrayscale( element ){  
 	  var filter = window.getComputedStyle(element,null).getPropertyValue("filter");
 	  if(filter.startsWith('grayscale')){
